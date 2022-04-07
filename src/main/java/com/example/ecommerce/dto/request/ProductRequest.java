4aaebf30e735b7158bd3e72bbb6c3be3ccea75d3
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-04-07 00:02:12 -0300

    edit ProductDTOs

--- src/main/java/com/example/ecommerce/dto/request/ProductRequest.java
+++ src/main/java/com/example/ecommerce/dto/request/ProductRequest.java
@@ -1,4 +1,14 @@
 package com.example.ecommerce.dto.request;
 
+import java.util.UUID;
+
 public class ProductRequest {
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean inStock;
+    private String size;
+    private String color;
+    private String brand;
+    private Integer quantity;
 }
